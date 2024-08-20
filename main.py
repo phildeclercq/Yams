4a17980f7b9f2b8460785324e6363b906e084bac
phide <phi.declercq@gmail.com>
2024-08-20 15:53:43 +0200

    Version initial

--- main.py
+++ main.py
@@ -2,29 +2,35 @@ from random import randint
 import array as arr
 
 def lancerDe():
-    de = randint(1,6)
-    return de
+    return randint(1,6)
 
 def compter(Tab,Nb):
     points = 0
     for Number1 in range(0, len(Tab)):
-        if Number1 == Nb :
-          points = points + Nb
+        if Tab[Number1] == Nb :
+
+            points = points + Nb
     return points
+def calculTotal(Grille):
+    total = 0
+    for i in range(1, len(Grille)):
+        if Grille[i] != "X":
+            total = total + int(Grille[i])
+    return total
+
 
 if __name__ == '__main__':
-    Name = input('Entre ton nom stp : ')
+    Name = "Toto" # input('Entre ton nom stp : ')
     Grille = [Name, "X", "X", "X", "X", "X", "X"]
-    print('Voici ta grille de Yams de ' + Name)
-    for Number in range(1,len(Grille)):
-        print(str(Grille[Number]))
+    print('Voici la grille de Yams de ' + Name)
 
-    tabDes = [lancerDe(), lancerDe(), lancerDe(), lancerDe(), lancerDe()]
-    print('Tu lances les dés, et tu obtiens : ')
-    print (str(tabDes[0]) + ' / ', str(tabDes[1]) + ' / ', str(tabDes[2]) + ' / ', str(tabDes[3]) + ' / ', str(tabDes[4]))
-    print ('Que fais tu ?')
-    choix1 = input()
-    if choix1 =='1':
-        print(compter(tabDes,1))
-    else:
-        print('mauvais choix!')
+    for Tour in range(1,len(Grille)):
+        tabDes = [lancerDe(), lancerDe(), lancerDe(), lancerDe(), lancerDe()]
+        print('Tu lances les dés, et tu obtiens : ')
+        print (str(tabDes[0]) + ' / ', str(tabDes[1]) + ' / ', str(tabDes[2]) + ' / ', str(tabDes[3]) + ' / ', str(tabDes[4]))
+        choix1 = input('Que fais tu ?' )
+        result = (compter(tabDes,int(choix1)))
+        Grille[int(choix1)] = result
+        for Number in range(1,len(Grille)):
+            print(str(Number) + ' : '  + str(Grille[Number]))
+        print("Total : " + str(calculTotal(Grille)))
